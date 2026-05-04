aa3cd8d00f53b44bf3919376a77f0e595168f8ed
PabloFalc <pablopatricio640@gmail.com>
2026-05-04 10:02:49 -0300

    feat [request]: dio configurado para receber requests

--- lib/core/api_client.dart
+++ lib/core/api_client.dart
@@ -1,10 +1,42 @@
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:cookie_jar/cookie_jar.dart';
+import 'package:path_provider/path_provider.dart';
 
 final dio = Dio();
 final cookieJar = CookieJar();
 
-void setupDio() {
-  dio.interceptors.add(CookieManager(cookieJar));
+class ApiClient {
+  final Dio dio;
+  ApiClient._(this.dio);
+
+  static Future<ApiClient> create() async {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: 'https://localhost:3100',
+        connectTimeout: const Duration(seconds: 5),
+        receiveTimeout: const Duration(seconds: 5),
+        headers: {'Content-Type': 'application/json'},
+      ),
+    );
+    final dir = await getApplicationDocumentsDirectory();
+
+    final cookieJar = PersistCookieJar(
+      storage: FileStorage('${dir.path}/cookie'),
+    );
+    dio.interceptors.add(CookieManager(cookieJar));
+
+    dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
+
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onError: (error, handler) {
+          if (error.response?.statusCode == 401) {}
+          return handler.next(error);
+        },
+      ),
+    );
+
+    return ApiClient._(dio);
+  }
 }
